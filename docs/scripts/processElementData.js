bf8b999a34722b154ea96c3daac4c6dd8d9fd8e7
Martyn Ellison <mhe504@york.ac.uk>
2017-08-05 17:51:07 +0100

    published website added

---
+++ docs/scripts/processElementData.js
@@ -0,0 +1,31 @@
+//------------------------------------------------------------------------------
+// Copyright (c) 2005, 2006 IBM Corporation and others.
+// All rights reserved. This program and the accompanying materials
+// are made available under the terms of the Eclipse Public License v1.0
+// which accompanies this distribution, and is available at
+// http://www.eclipse.org/legal/epl-v10.html
+// 
+// Contributors:
+// IBM Corporation - initial implementation
+//------------------------------------------------------------------------------
+
+// This file will include dynamically generated process layout related data from 
+// the publishing service 
+contentPage.processPage.imageFiles["_-l1BsXOQEeeXYNQ0s8JyrgActivity"]="./../../migration_opition_evaluation/deliveryprocesses/resources/methodology_CA247006_ca247006_Activity.jpeg"
+contentPage.processPage.imageFiles["_-l1BsXOQEeeXYNQ0s8Jyrg,_gy3SwXOREeeXYNQ0s8JyrgActivity"]="./../../migration_opition_evaluation/deliveryprocesses/resources/Modelling_CBEC4CE5_f527193f_Activity.jpeg"
+contentPage.processPage.imageFiles["_-l1BsXOQEeeXYNQ0s8Jyrg,_3J3xAXOREeeXYNQ0s8JyrgActivity"]="./../../migration_opition_evaluation/deliveryprocesses/resources/Simulation_6B87C045_94c28c9f_Activity.jpeg"
+contentPage.processPage.imageFiles["_-l1BsXOQEeeXYNQ0s8Jyrg,_TGWfcXOREeeXYNQ0s8JyrgActivity"]="./../../migration_opition_evaluation/deliveryprocesses/resources/Planning_D5213C29_fe5c0883_Activity.jpeg"
+contentPage.processPage.elementUrls["_3J3xAXOREeeXYNQ0s8Jyrg"] = ["Simulation","migration_opition_evaluation/deliveryprocesses/Simulation_6B87C045.html"];
+contentPage.processPage.elementUrls["_GYAvIHOSEeeXYNQ0s8Jyrg"] = ["Execute Simulations","migration_opition_evaluation/deliveryprocesses/execute_simulations_2E424819.html"];
+contentPage.processPage.elementUrls["_kVN_MHOREeeXYNQ0s8Jyrg"] = ["Schema Extraction","migration_opition_evaluation/deliveryprocesses/schema_extraction_F49ADEA9.html"];
+contentPage.processPage.elementUrls["_gy3SwXOREeeXYNQ0s8Jyrg"] = ["Modelling","migration_opition_evaluation/deliveryprocesses/Modelling_CBEC4CE5.html"];
+contentPage.processPage.elementUrls["_TGWfcXOREeeXYNQ0s8Jyrg"] = ["Planning","migration_opition_evaluation/deliveryprocesses/Planning_D5213C29.html"];
+contentPage.processPage.elementUrls["_-l1BsXOQEeeXYNQ0s8Jyrg"] = ["Option Evaluation Methodology","migration_opition_evaluation/deliveryprocesses/methodology_CA247006.html"];
+contentPage.processPage.elementUrls["_eeL6YHOREeeXYNQ0s8Jyrg"] = ["Search for Workload Information Sources","migration_opition_evaluation/deliveryprocesses/search_for_workload_information_sources_1B8264B.html"];
+contentPage.processPage.elementUrls["_Ubl0EHOREeeXYNQ0s8Jyrg"] = ["Select Simulation Goal","migration_opition_evaluation/deliveryprocesses/select_simulation_goal_3A6872C6.html"];
+contentPage.processPage.elementUrls["_bPM14HOREeeXYNQ0s8Jyrg"] = ["Determine Security Requirements","migration_opition_evaluation/deliveryprocesses/determine_security_requirements_DFD311F8.html"];
+contentPage.processPage.elementUrls["_DD0agHOSEeeXYNQ0s8Jyrg"] = ["Determine Database Performance","migration_opition_evaluation/deliveryprocesses/determine_database_performance_5A843325.html"];
+contentPage.processPage.elementUrls["_76iqsHOREeeXYNQ0s8Jyrg"] = ["Create Cloud Cost Model(s)","migration_opition_evaluation/deliveryprocesses/create_cloud_cost_model_2006526E.html"];
+contentPage.processPage.elementUrls["_uiE0wHOREeeXYNQ0s8Jyrg"] = ["Obtain Workload Data","migration_opition_evaluation/deliveryprocesses/obtain_workload_data_7E24FEC2.html"];
+contentPage.processPage.elementUrls["_xhIm4HOREeeXYNQ0s8Jyrg"] = ["Model Transformation","migration_opition_evaluation/deliveryprocesses/model_transformation_330FAF7A.html"];
+contentPage.processPage.elementUrls["_CVZQEHXIEeeXYNQ0s8Jyrg"] = ["Query Logging","migration_opition_evaluation/deliveryprocesses/query_logging_2CD5694D.html"];
